e762b650b953ce88823123612b459ce0f3683876
Punam Dahiya <punamdahiya@yahoo.com>
2018-04-18 09:05:57 -0700

    Handle onInstall and onStartup

--- webextension/background.js
+++ webextension/background.js
@@ -1,9 +1,24 @@
 try {
-  browser.cloudstorage.init(browser.runtime.getURL("./skin/clouddownloads.css")).then(
-    result => {
-      console.log(result);
-    }
-  );
+  function handleStartup() {
+  	console.log("startup called");
+  	 browser.cloudstorage.init(browser.runtime.getURL("./skin/clouddownloads.css")).then(
+	    result => {
+	      console.log(result);
+	    }
+	  );
+  }
+
+  function handleInstalled() {
+  	console.log("Installed called");
+  	 browser.cloudstorage.init(browser.runtime.getURL("./skin/clouddownloads.css")).then(
+	    result => {
+	      console.log(result);
+	    }
+	  );
+  }
+
+  browser.runtime.onStartup.addListener(handleStartup);
+  browser.runtime.onInstalled.addListener(handleInstalled);
 } catch(e) {
   console.log(e);
 }
